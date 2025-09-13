f907e38112ad284bd666005875acf4b6b525d168
Andrea Aguirre <aguirreandrea457@gmail.com>
2025-09-12 21:18:52 -0500

    avance

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -1,7 +1,24 @@
+import "../style/Contenido.css";
+import Musica from "./Musica";
+
 export default function Contenido() {
-    return(
-        <>
-        
-        </>
-    );
+  return (
+    <>
+      <div className="container_texto_invitacion">
+        <div className="img_logo">
+          <img
+            src="https://res.cloudinary.com/dnao6nouz/image/upload/v1757729575/image_17_1_pyrbqg.png"
+            alt=""
+          />
+          <div className="nombre">
+            <h1>Chris</h1>
+          </div>
+        </div>
+
+        <div className="musica_reproductor">
+          <Musica />
+        </div>
+      </div>
+    </>
+  );
 }
